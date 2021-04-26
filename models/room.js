560d3328940212c09962ed03150f3feedf03c8fa
Anshumani Sharma <anshumani2001@gmail.com>
2021-04-26 12:24:31 +0530

    Created express app, connected with mongodb

---
+++ models/room.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RoomSchema = new Schema({
+    name: String,
+    price: String,
+    maxOccupants: Number,
+    description: String,
+    location: String,
+    attachedBathroom: Boolean,
+    attachedBalcony: Boolean
+})
+
+module.exports = mongoose.model('Room',RoomSchema);
